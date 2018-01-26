122fbec124cdaeaa4120ad86856858d3ec01a85e
Eliab Woldeyes <woldeyes.eliab@hotmail.com>
2018-01-26 13:45:24 -0500

    Robot states enum defined

---
+++ SketchProgram/RobotStates.h
@@ -0,0 +1,16 @@
+/*	States to help Robot Robot control
+*/
+
+#ifndef ROBOT_STATES
+	#define ROBOT_STATES
+
+	//Some sample states
+	enum state {
+		STOP,
+		LEFT,
+		RIGHT,
+		T_JUNCTION,
+		CROSS_JUNCTION
+	}
+
+#endif
