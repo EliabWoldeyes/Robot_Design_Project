15c1ef17fa35fa04321acd744ce9cbc4ec319923
EliabWoldeyes <woldeyes.eliab@hotmail.com>
2018-02-02 16:22:44 -0500

    Updated Lab work

---
+++ SketchProgram/RobotStates.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "RobotStates.h"
+#include "SensorControl.h"
+
+//Returns a state value depending on last state and current sensor data
+State determineRobotState(){
+  return STOP;
+}
+
+//Update the new Robot state setting:
+//      previous state  -->   current state
+//      current state   -->   new state
+void updateRobotState(RobotState curState, State newState){
+  curState.previousState = curState.currentState;
+  curState.currentState = newState;
+}
+
---
+++ SketchProgram/SensorControl.h
@@ -0,0 +1,20 @@
+/*	All function prototypes
+	defined in SensorControl.cpp
+*/
+
+#ifndef	SENSOR_CONTROL_FUNC
+	#define SENSOR_CONTROL_FUNC
+
+  // Black is low from sensor, but digitalRead as high
+  // White is high from sensor, but digitalRead as low
+
+	// Pins for sensor inputs
+	const int right_sens = 7;
+	const int left_sens = 8;
+	
+	boolean leftLow();
+	boolean leftHigh();
+	boolean rightLow();
+	boolean rightHigh();
+	
+#endif
