e3430e600eb37fcfd6f08acd9acc85fadcc5bee2
EliabWoldeyes <woldeyes.eliab@hotmail.com>
2018-02-02 16:22:44 -0500

    Updated Lab work

---
+++ SketchProgram/SensorControl.cpp
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "SensorControl.h"
+
+
+// Black is low from sensor, but digitalRead as high
+// White is high from sensor, but digitalRead as low
+
+//left sensor reading LOW
+boolean leftLow(){
+	return digitalRead(left_sens) == LOW;
+}
+
+//left sensor reading HIGH
+boolean leftHigh(){
+	return digitalRead(left_sens) == HIGH;
+}
+
+//right sensor reading LOW
+boolean rightLow(){
+	return digitalRead(right_sens) == LOW;
+}
+
+//right sensor reading HIGH
+boolean rightHigh(){
+	return digitalRead(right_sens) == HIGH;
+}
